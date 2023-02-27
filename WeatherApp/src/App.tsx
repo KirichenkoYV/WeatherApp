6bc0d3407fc34e06c8626317f3f86ff59863b1a8
KirichenkoYV <hause99@gmail.com>
2023-02-27 19:38:47 +0300

    headerDone

--- WeatherApp/src/App.tsx
+++ WeatherApp/src/App.tsx
@@ -1,11 +1,18 @@
+import React from "react";
+import { Route, Routes } from "react-router";
+import Header from "./overall/Header/Header";
+import HomePage from "./pages/HomePage/components/HomePage";
 
 
 function App() {
-
   return (
-    <div className="App">
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<HomePage></HomePage>} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ WeatherApp/src/pages/HomePage/components/HomePage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./HomePage.module.scss";
+
+function HomePage() {
+  return <div className={styles.home}>Home</div>;
+}
+
+export default HomePage;
